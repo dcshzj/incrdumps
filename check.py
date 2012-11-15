9ddd6907c915d8fc0f9d999a8e6abe77cb9e395e
Hydriz <admin@alphacorp.tk>
2012-11-15 12:25:25 +0800

    Adding the checker script.

---
+++ check.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from datetime import datetime, timedelta
+import os
+import sys
+
+class IncrChecker:
+	def __init__(self):
+		workingdate = datetime.today() - timedelta(2)
+		self.date = workingdate.strftime('%Y%m%d')
+		self.filetouse = "status.txt"
+
+	def dldtestfile(self, wiki):
+		os.system("wget -cq http://archive.org/download/incr-%s-%s/%s" % (wiki, self.date, self.filetouse))
+
+	def reupload(self, wiki):
+		os.system("python incrdumps.py %s %s" % (self.date, wiki))
+
+	def test(self, wiki):
+		self.dldtestfile(wiki)
+		if (os.path.exists(self.filetouse)):
+			os.system("rm %s" % (self.filetouse))
+		else:
+			os.system("rm %s" % (self.filetouse))
+			self.reupload(wiki)
+
+	def main(self):
+		wikilist = "%s-wikis.txt" % (self.date)
+		wikis = open(wikilist, 'r').read().splitlines()
+		for wiki in wikis:
+			print "Checking %s..." % (wiki)
+			self.test(wiki)
+
+def main():
+	x = IncrChecker()
+	x.main()
+	print "Done!"
+
+if __name__ == "__main__":
+	main()
