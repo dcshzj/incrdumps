bbbdd12e8867ffd94bd354270e7c03197059687d
Hydriz <admin@alphacorp.tk>
2013-10-20 19:58:37 +0800

    Fix call for variable

--- incrdumps.py
+++ incrdumps.py
@@ -82,7 +82,7 @@ def foreachwiki():
 		x = converter.ASConverter()
 		x.convertdate(userdate)
 		curdate = x.date
-		d = datetime.strptime(curdate, '%Y%m%d')
+		d = datetime.strptime(userdate, '%Y%m%d')
 		archivedate = d.strftime('%Y-%m-%d')
 		if (curwiki == "Here's the big fat disclaimer."): # The only non-wiki string that is in bold
 			continue
